cc89bace5f9cad3610e41ba574bd7d4a0adb0ca3
zolot <zolot3437@gmail.com>
2015-11-05 13:11:21 +0200

    +

--- js/common.js
+++ js/common.js
@@ -40,23 +40,12 @@ $(document).ready(function() {
     })
 
     $('.works-popup').magnificPopup({
-         mainClass: 'mfp-with-zoom', // this class is for CSS animation below
+        // Delay in milliseconds before popup is removed
          removalDelay: 300,
-         zoom: {
-           enabled: true, // By default it's false, so don't forget to enable it
 
-           duration: 300, // duration of the effect, in milliseconds
-           easing: 'ease-in-out', // CSS transition easing function
-
-           // The "opener" function should return the element from which popup will be zoomed in
-           // and to which popup will be scaled down
-           // By defailt it looks for an image tag:
-           opener: function(openerElement) {
-             // openerElement is the element on which popup was initialized, in this case its <a> tag
-             // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-             return openerElement.is('img') ? openerElement : openerElement.find('img');
-           }
-         }
+         // Class that is added to popup wrapper and background
+         // make it unique to apply your CSS animations just to this exact popup
+         mainClass: 'mfp-fade'
     });
     
     $('.popup').magnificPopup({
